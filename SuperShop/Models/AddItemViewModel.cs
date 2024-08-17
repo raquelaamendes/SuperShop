7464999a88787801b8055312e81206257f1385d6
raquelaamendes <raquelalexandramendes@gmail.com>
2024-08-17 10:34:18 +0100

    Products combo

---
+++ SuperShop/Models/AddItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SuperShop.Models
+{
+    public class AddItemViewModel
+    {
+        [Display(Name = "Product")]
+        [Range(1, int.MaxValue, ErrorMessage = "You must select a product.")] //Começa no 1 porque o 0 não será nenhum produto selecionavel
+        public int ProductId { get; set; }
+
+
+
+        [Range(0.0001, double.MaxValue, ErrorMessage = "The quantity must be a positive number.")]
+        public double Quantity { get; set; }
+
+
+
+        public IEnumerable<SelectListItem> Products { get; set; }
+    }
+}
