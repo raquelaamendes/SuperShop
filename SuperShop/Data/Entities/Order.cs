6acea09b73b2dc0e47ca07b36f2cfb19a7f5c354
raquelaamendes <raquelalexandramendes@gmail.com>
2024-08-22 14:22:57 +0100

    Confirm orders

--- SuperShop/Data/Entities/Order.cs
+++ SuperShop/Data/Entities/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -25,10 +26,17 @@ namespace SuperShop.Data.Entities
 
         public IEnumerable<OrderDetail> Items { get; set; }
 
+        [DisplayFormat(DataFormatString ="{0:N0}")]
+        public int Lines => Items == null ? 0 : Items.Count();
+
         [DisplayFormat(DataFormatString = "{0:N2}")]
         public double Quantity => Items == null ? 0 : Items.Sum(i => i.Quantity);
 
         [DisplayFormat(DataFormatString = "{0:C2}")]
         public decimal Value => Items == null ? 0 : Items.Sum(i => i.Value);
+
+        [Display(Name = "Order date")]
+        [DisplayFormat(DataFormatString = "{0:mm/dd/yyyy HH:mm}", ApplyFormatInEditMode = false)]
+        public DateTime? OrderDateLocal => this.OrderDate == null ? null : this.OrderDate.ToLocalTime();
     }
 }
