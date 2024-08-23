f4460517479bf7a41c4cd5c4396447d483e24561
raquelaamendes <raquelalexandramendes@gmail.com>
2024-08-23 15:13:08 +0100

    Cascade Delete Rule

--- SuperShop/Controllers/ProductsController.cs
+++ SuperShop/Controllers/ProductsController.cs
@@ -53,7 +53,7 @@ namespace SuperShop.Controllers
         }
 
         // GET: Products/Create
-        [Authorize(Roles = "Admin")] //Indicar o role que pode fazer esta ação
+        //[Authorize(Roles = "Admin")] //Indicar o role que pode fazer esta ação
         //[Authorize(Roles = "Admin, Customer, ...")] - Como fazer para vários roles
         public IActionResult Create()
         {
@@ -165,11 +165,30 @@ namespace SuperShop.Controllers
         // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var product = await _productRepository.GetByIdAsync(id);
-            await _productRepository.DeleteAsync(product);
-            return RedirectToAction(nameof(Index));
+
+            try
+            {
+                //throw new Exception("Excepção de Teste");
+                await _productRepository.DeleteAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException ex) 
+            {
+                if (ex.InnerException != null && ex.InnerException.Message.Contains("DELETE"))
+                {
+                    ViewBag.ErrorTitle = $"{product.Name} provavelmente está a ser usado!!";
+                    ViewBag.ErrorMessage = $"{product.Name} não pode ser apagado visto haverem encomendas que o usam.</br></br>" +
+                        $"Experimente primeiro apagar todas as encomendas que o estão a usar," +
+                        $"e torne novamente a apagá-lo"; 
+                }
+
+                return View("Error");
+            }
+
         }
 
         public IActionResult ProductNotFound()
