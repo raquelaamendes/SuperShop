e5ac1de3992215376d6a8d503641b3b70d926c99
raquelaamendes <raquelalexandramendes@gmail.com>
2024-08-27 23:26:15 +0100

    Confirm email registration

--- SuperShop/Startup.cs
+++ SuperShop/Startup.cs
@@ -27,6 +27,8 @@ namespace SuperShop
         {
             services.AddIdentity<User, IdentityRole>(cfg =>
             {
+                cfg.Tokens.AuthenticatorTokenProvider = TokenOptions.DefaultAuthenticatorProvider;
+                cfg.SignIn.RequireConfirmedEmail = true;
                 cfg.User.RequireUniqueEmail = true;
                 cfg.Password.RequireDigit = false;
                 cfg.Password.RequiredUniqueChars = 0;
@@ -35,6 +37,7 @@ namespace SuperShop
                 cfg.Password.RequireNonAlphanumeric = false;
                 cfg.Password.RequiredLength = 6;
             })
+                .AddDefaultTokenProviders()
                 .AddEntityFrameworkStores<DataContext>();
 
             services.AddAuthentication()
@@ -64,6 +67,8 @@ namespace SuperShop
             services.AddScoped<IBlobHelper, BlobHelper>();
 
             services.AddScoped<IConverterHelper, ConverterHelper>();
+
+            services.AddScoped<IMailHelper, MailHelper>();
 
 
             services.AddScoped<IProductRepository, ProductRepository>();
